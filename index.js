f560bf6d83502e33785ae05ee33ef71802af738c
Atharva <dummy@gmail.com>
2025-03-17 14:29:31 +0530

    resolved an error

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const app = express();
 const path = require("path");
 const PORT = 8080;
 
+// Middleware to parse URL-encoded form data
 app.use(express.urlencoded({ extended: true }));
 
+// Set EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "public")));
+
+// Correct way to serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Server working well!");
 });
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
