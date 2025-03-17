4a80fb667a6514654f7d5259b0bd96410152265b
Atharva <dummy@gmail.com>
2025-03-17 17:18:21 +0530

    Completed Edit and Delete Route

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 const path = require("path");
 const PORT = 8080;
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 // Middleware to parse URL-encoded form data
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
@@ -51,6 +54,28 @@ app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   let post = posts.find((p) => id === p.id);
   res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  console.log(post);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+//   res.send("delete success");
+  res.redirect("/posts");
 });
 
 app.get("/", (req, res) => {
