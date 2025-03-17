5f99730408555fae3dfb99065f75f2d7a4064f74
Atharva <dummy@gmail.com>
2025-03-17 14:55:11 +0530

    index route created

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ app.set("views", path.join(__dirname, "views"));
 // Correct way to serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+let posts = [
+  {
+    username: "atharva",
+    content: "I love coding",
+  },
+  {
+    username: "suhaib",
+    content: "Hard work is the key to success",
+  },
+  {
+    username: "madhav",
+    content: "I got selected for my first internship!",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
 app.get("/", (req, res) => {
   res.send("Server working well!");
 });
